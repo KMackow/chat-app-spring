6bcff84b61affa0852ba8f61cf24a3d12a2a3c9b
KMackow <kamilmackow@hotmail.co.uk>
2018-10-09 14:58:23 +0100

    configured Oauth2 and REST to work with the basic chat app (login/logout/get users)

--- src/main/java/chat/server/ServerApplication.java
+++ src/main/java/chat/server/ServerApplication.java
@@ -1,12 +1,31 @@
 package chat.server;
 
+import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeaderBindingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class ServerApplication {
+public class ServerApplication extends WebSecurityConfigurerAdapter  {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/bundle.js", "/favicon.ico", "/login**", "/callback", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.addFilterAfter(new CsrfTokenResponseHeaderBindingFilter(), CsrfFilter.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
--- src/main/java/chat/server/ServerController.java
+++ src/main/java/chat/server/ServerController.java
@@ -1,16 +1,22 @@
 package chat.server;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RequestMapping(value = "/api")
 @RestController
 public class ServerController {
 
@@ -18,6 +24,13 @@ public class ServerController {
 
     ServerController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user")
+    public User user(OAuth2Authentication authentication) {
+        Map<String, String> userDetails = (Map<String, String>) authentication.getUserAuthentication().getDetails();
+        User newUser = new User(userDetails.get("login"), userDetails.get("name"), userDetails.get("avatar_url"));
+        return userRepository.save(newUser);
     }
 
     @GetMapping("/user{id}")
